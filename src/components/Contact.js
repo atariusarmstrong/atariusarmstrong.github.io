4364c1c0f62f723956c259c7f9391afa214f6b34
Atarius Armstrong <atariusarmstrong@gmail.com>
2019-03-11 13:12:59 -0400

    added social media icons

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,12 +2,19 @@ import React, { Component } from 'react';
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+import {SocialIcon} from 'react-social-icons'
 
 class Contact extends Component {
     render() {
         return (
             <footer className='contactBox'>
                 <Container>
+                    <Row className="justify-content-md-center">
+                        <SocialIcon url="http://linkedin.com/in/atariusarmstrong" bgColor='#ACACAC' className='socialIcons' style={{height: 25, width: 25}}/>
+                        <SocialIcon url='http://instagram.com/atariusarmstrong' bgColor='#ACACAC' className='socialIcons' style={{height: 25, width: 25}}/>
+                        <SocialIcon url='http://facebook.com/atariuslamon' bgColor='#ACACAC' className='socialIcons' style={{height: 25, width: 25}}/>
+                        <SocialIcon url='http://github.com/atariusarmstrong' bgColor='#ACACAC' className='socialIcons' style={{height: 25, width: 25}}/>
+                    </Row>
                     <Row>
                         <Col>Atarius Armstrong</Col>
                         <Col>Full Stack, UI Web Developer</Col>
