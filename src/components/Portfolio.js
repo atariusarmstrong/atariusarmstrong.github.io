e3bfa527deaeacbb8f6dd38293d8112c395a9ac2
Atarius Armstrong <atariusarmstrong@gmail.com>
2019-03-11 10:54:10 -0400

    made a carousel for projects

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -4,6 +4,9 @@ import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import { keyframes } from 'popmotion';
+import makeCarousel from 'react-reveal/makeCarousel'
+import Slide from 'react-reveal/Slide'
+import PortfolioCarousel from './PortfolioCarousel';
 
 //I want make each div move from left to right
 //Once it reaches the right it should disappear and show the next project
@@ -45,27 +48,6 @@ const Skill = posed.li({
 
 class Portfolio extends Component {
     state = {
-        projects: [{
-            title: "Jeopardy",
-            description: "A static HTML/CSS/JS replica of the game show jeopardy.",
-            url: 'https://github.com/atariusarmstrong/unit1Project',
-            image: ''
-        },
-        {
-            title: "Bartel",
-            description: "Fullstack application using Express & Node.js where users can log their favorite bars to day drink.",
-            url: 'https://github.com/atariusarmstrong/bartel'
-        },
-        {
-            title: "TripShare",
-            description: "Fullstack application utilizing Mongo, Express, React.js, and Node.js for users to log future group travel plans.",
-            url: 'https://github.com/atariusarmstrong/trip-share'
-        },
-        {
-            title: 'REA Entertainment',
-            description: 'Fullstack application using React.js and Django for a mock website for a theatrical production company.',
-            url: 'https://github.com/atariusarmstrong/rea_entertainment'
-        }],
         position: true,
         isOpen: false
     }
@@ -95,16 +77,7 @@ class Portfolio extends Component {
                         </Col>
                         
                         <Col className='projectContainer'>
-                            {this.state.projects.map((project, i) => {
-                                return (
-                                    <a href={project.url}>
-                                        <Project key={i} className='projectBox' pose={this.state.position ? 'left' : 'right'}>
-                                            <h3>{project.title}</h3>
-                                            <p>{project.description}</p>
-                                        </Project>
-                                    </a>
-                            )
-                            })}
+                            <PortfolioCarousel/>
                         </Col>
                     </Row>
                 </Container>
---
+++ src/components/PortfolioCarousel.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import makeCarousel from 'react-reveal/makeCarousel'
+import Slide from 'react-reveal/Slide'
+import styled from 'styled-components'
+
+const Container = styled.div`
+    position: relative;
+    overflow: hidden;
+    width: 700px;
+    height: 500px;
+`
+
+const CarouselUI = ({ children }) => <Container>{children}</Container>;
+const Carousel = makeCarousel(CarouselUI);
+
+class PortfolioCarousel extends Component {
+    state = {
+        projects: [{
+            title: "Jeopardy",
+            description: "A static HTML/CSS/JS replica of the game show jeopardy.",
+            url: 'https://github.com/atariusarmstrong/unit1Project',
+            image: ''
+        },
+        {
+            title: "Bartel",
+            description: "Fullstack application using Express & Node.js where users can log their favorite bars to day drink.",
+            url: 'https://github.com/atariusarmstrong/bartel'
+        },
+        {
+            title: "TripShare",
+            description: "Fullstack application utilizing Mongo, Express, React.js, and Node.js for users to log future group travel plans.",
+            url: 'https://github.com/atariusarmstrong/trip-share'
+        },
+        {
+            title: 'REA Entertainment',
+            description: 'Fullstack application using React.js and Django for a mock website for a theatrical production company.',
+            url: 'https://github.com/atariusarmstrong/rea_entertainment'
+        }]
+    }
+    render() {
+        return (
+            <div>
+                <Carousel defaultWait={5000} >
+                    {this.state.projects.map((project, i ) => {
+                        return (
+                                <Slide right>
+                                
+                                    <div key={i}>
+                                        <a href={project.url}>
+                                            <h3>{project.title}</h3>
+                                            <p>{project.description}</p>
+                                        </a>
+                                    </div>
+                                
+                                </Slide>
+                        )
+                    })}
+                </Carousel>
+            </div>
+        );
+    }
+}
+
+export default PortfolioCarousel;
