adad3cba43020a4fd123de89f28e296d424add87
Atarius Armstrong <atariusarmstrong@gmail.com>
2019-03-11 22:46:14 -0400

    attempting carousel with react-bootstrap carousel instead of react reveal

--- src/components/PortfolioCarousel.js
+++ src/components/PortfolioCarousel.js
@@ -1,21 +1,22 @@
 import React, { Component } from 'react';
+import Carousel from 'react-bootstrap/Carousel'
 import makeCarousel from 'react-reveal/makeCarousel'
 import Slide from 'react-reveal/Slide'
 import styled from 'styled-components'
 
-const Container = styled.div`
-    position: relative;
-    overflow: hidden;
-    width: 700px;
-    height: 500px;
-    img {
-        width: 700px;
-        height: 500px;
-    }
-`
+// const Container = styled.div`
+//     position: relative;
+//     overflow: hidden;
+//     width: 700px;
+//     height: 500px;
+//     img {
+//         width: 700px;
+//         height: 500px;
+//     }
+// `
 
-const CarouselUI = ({ children }) => <Container>{children}</Container>;
-const Carousel = makeCarousel(CarouselUI);
+// const CarouselUI = ({ children }) => <Container>{children}</Container>;
+// const Carousel = makeCarousel(CarouselUI);
 
 class PortfolioCarousel extends Component {
     state = {
@@ -47,7 +48,7 @@ class PortfolioCarousel extends Component {
     render() {
         return (
             <div>
-                <Carousel defaultWait={5000} className='carousel'>
+                {/* <Carousel defaultWait={5000} className='carousel'>
                     {this.state.projects.map((project, i ) => {
                         return (
                                 <Slide right>
@@ -55,12 +56,28 @@ class PortfolioCarousel extends Component {
                                     <div key={i}>
                                         <a href={project.url}>
                                             <h3>{project.title}</h3>
-                                            <img src={project.image} atl={project.title}/>
+                                            <img src={project.image} alt={project.title}/>
                                             <p>{project.description}</p>
                                         </a>
                                     </div>
                                 
                                 </Slide>
+                        )
+                    })}
+                </Carousel> */}
+                <Carousel>
+                    {this.state.projects.map((project, i) => {
+                        return (
+                            <Carousel.Item key={i}>
+                                <a href={project.url}>
+                                    
+                                    <img src={project.image} alt={project.title} className="d-block w-100"/>
+                                    <Carousel.Caption>
+                                        <h3>{project.title}</h3>
+                                        <p>{project.description}</p>
+                                    </Carousel.Caption>   
+                                </a>
+                            </Carousel.Item>
                         )
                     })}
                 </Carousel>
